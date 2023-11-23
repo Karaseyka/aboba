5c3bb41deabfee563cdf06c6dda4bf0e9dea71a4
Karaseyka <vladchuprina12@mail.ru>
2023-11-23 17:56:26 +0300

    aboba2

--- main.py
+++ main.py
@@ -5,15 +5,18 @@ from PyQt5 import uic
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow
 
+from ui_file import Ui_MainWindow
 
-class Example(QMainWindow):
+
+class Example(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.do_paint = False
         self.initUI()
 
     def initUI(self):
-        uic.loadUi("UI.ui", self)
+        # uic.loadUi("UI.ui", self)
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.paint)
 
     def paint(self):
@@ -29,8 +32,8 @@ class Example(QMainWindow):
         self.do_paint = False
 
     def draw_flag(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
         for i in range(100):
+            qp.setBrush(QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
             x = random.randint(20, 600)
             y = random.randint(20, 600)
             r = random.randint(10, 100)
